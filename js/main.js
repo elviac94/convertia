b67ce3d14293b1b8282147d89c2657d45eeca8ad
elviac94 <elvi_ac94@hotmail.com>
2020-08-13 13:24:30 +0200

    add offers structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 'use-strict';
+const phoneIcon = document.querySelector('.phone__container');
+const home = document.querySelector('.main__home');
+const contractSection = document.querySelector('.contract__container');
+
+const getContractSection = () =>{
+    
+    if(!home.classList.contains('hidden')){
+        home.classList.add('hidden')
+        contractSection.classList.remove('hidden')
+    }
+}
+
+phoneIcon.addEventListener('click',getContractSection);

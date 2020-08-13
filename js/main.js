b2f93527a7c33813de1da3e1f971cc262f85f7b6
elviac94 <elvi_ac94@hotmail.com>
2020-08-13 15:08:42 +0200

    add corrections in js and more styles in contract page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 'use-strict';
 const phoneIcon = document.querySelector('.phone__container');
+const contractButton = document.querySelector('.contract__button');
 const home = document.querySelector('.main__home');
 const contractSection = document.querySelector('.contract__container');
 
 const getContractSection = () =>{
-    
+
     if(!home.classList.contains('hidden')){
         home.classList.add('hidden')
         contractSection.classList.remove('hidden')
     }
 }
 
-phoneIcon.addEventListener('click',getContractSection);
+contractButton.addEventListener('click',getContractSection);
